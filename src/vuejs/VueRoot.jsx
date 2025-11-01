8c3fc524d46b17429898c990a9cb650816ddbec5
jainamsheth1234-ship-it <jainamsheth1234@gmail.com>
2025-11-01 19:29:55 +0100

    added the fixed parts

---
+++ src/vuejs/VueRoot.jsx
@@ -0,0 +1,11 @@
+import { Summary }  from "./summaryPresenter.jsx";
+
+
+function VueRoot(props){
+    return (<div>
+                <div><Summary model={props.model} /></div>
+            </div>
+           );
+}
+
+export { VueRoot }
